7aa12b3509a546080727a20b2b48f1dcbf6acaf3
tim <tim@Kugane.local>
2024-12-07 20:27:05 +0100

    day7 part 1

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -3,13 +3,33 @@ package day07
 import util.*
 
 fun main() {
-	val input = readInput("day07")
+	val input = readInput("day07_test")
 	part1(input)
 	part2(input)
 }
 
 fun part1(input: List<String>) {
+	var correct = 0L
+	input.forEach() {
+		val parts = it.split(":\\s+".toRegex())
+		val target = parts[0].toLong()
+		val eqsolver = EqSolver(target)
+		val numbers = parts[1].split(" ").map { v -> v.toLong()}.toMutableList()
+		val first = numbers.removeAt(0)
+		if (eqsolver.SolveFor(first, numbers)) correct += target
+	}
+	println(correct)
 }
 
 fun part2(input: List<String>) {
+	var correct = 0L
+	input.forEach() {
+		val parts = it.split(":\\s+".toRegex())
+		val target = parts[0].toLong()
+		val eqsolver = EqSolver(target)
+		val numbers = parts[1].split(" ").map { v -> v.toLong()}.toMutableList()
+		val first = numbers.removeAt(0)
+		if (eqsolver.SolveForConcat(first, numbers)) correct += target
+	}
+	println(correct)
 }
--- src/day07/EqSolver.kt
+++ src/day07/EqSolver.kt
@@ -1,10 +1,11 @@
 package day07
 
-class EqSolver(val target: Int) {
-	val plus: (Int, Int) -> Int = { a, b -> a+b }
-	val times: (Int, Int) -> Int = { a, b -> a*b }
+class EqSolver(val target: Long) {
+	val plus: (Long, Long) -> Long = { a, b -> a+b }
+	val times: (Long, Long) -> Long = { a, b -> a*b }
+	val concat: (Long, Long) -> Long = { a, b -> a*(10*(b/10)) + b }
 	
-	fun SolveFor(i: Int, next: MutableList<Int>): Boolean {
+	fun SolveFor(i: Long, next: MutableList<Long>): Boolean {
 		if (next.isEmpty()) {
 			if (i == target) return true else return false
 		}
@@ -16,5 +17,28 @@ class EqSolver(val target: Int) {
 		if (times(i, first) > target) return SolveFor(plus(i, first), newNext)
 		if (plus(i, first) > target) return SolveFor(times(i, first), newNext)
 		return SolveFor(times(i, first), newNext) || SolveFor(plus(i, first), newNext)
+	}
+	
+	fun SolveForConcat(i: Long, next: MutableList<Long>): Boolean {
+		if (next.isEmpty()) {
+			if (i == target) return true else return false
+		}
+		val newNext = next.toMutableList()
+		val first = newNext.removeAt(0)
+		if (concat(i, first) > target && times(i, first) > target && plus(i, first) > target) {
+			return false
+		}
+		if (concat(i, first) > target && times(i, first) > target) return SolveForConcat(plus(i, first), newNext)
+		if (concat(i, first) > target && plus(i, first) > target) return SolveForConcat(times(i, first), newNext)
+		if (times(i, first) > target && plus(i, first) > target) return SolveForConcat(concat(i, first), newNext)
+		
+		if (concat(i, first) > target) return SolveForConcat(times(i, first), newNext) || SolveForConcat(plus(i, first), newNext)
+		if (times(i, first) > target) return SolveForConcat(concat(i, first), newNext) || SolveForConcat(plus(i, first), newNext)
+		if (plus(i, first) > target) return SolveForConcat(concat(i, first), newNext) || SolveForConcat(times(i, first), newNext)
+		return SolveForConcat(concat(i, first), newNext) || SolveForConcat(times(i, first), newNext) || SolveForConcat(plus(i, first), newNext)
+	}
+	
+	fun Test(a: Long, b: Long): Long {
+		return 
 	}
 }
